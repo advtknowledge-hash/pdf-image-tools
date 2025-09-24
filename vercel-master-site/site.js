53ed76f927d171c3844424daa84f65b5348da14d
advtknowledge-hash <advtknowledge@gmail.com>
2025-09-24 13:22:48 +0530

    site.js

--- vercel-master-site/site.js
+++ vercel-master-site/site.js
@@ -3,6 +3,9 @@
   document.querySelectorAll('.tool-tabs a').forEach(function(a){
     var p = new URL(a.getAttribute('href'), location.href).pathname
               .replace(/\/index\.html?$/,'/').toLowerCase();
-    if (p === cur) a.classList.add('active');
+    // also handle case with/without .html
+    if (p === cur || p + '.html' === cur || p === cur + '.html') {
+      a.classList.add('active');
+    }
   });
 })();
